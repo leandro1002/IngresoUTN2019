5450774d7286492eeb85a8c8f06c497b6cf115be
leandro1002 <47338161+leandro1002@users.noreply.github.com>
2019-02-25 19:52:16 -0300

    Add files via upload

---
+++ jsIteraciones (6).js
@@ -0,0 +1,23 @@
+function Mostrar()
+{
+
+  	var contador=0;
+	var acumulador=0;
+	var numero;
+	var promedio;
+	var suma;
+	
+
+	while (contador < 5){
+	numero = parseInt(prompt("Ingrese un numero: "));
+	contador ++;
+
+
+	}
+acumulador = acumulador + numero;
+
+
+document.getElementById('suma').value=acumulador;
+document.getElementById('promedio').value=acumulador/5;
+
+}//FIN DE LA FUNCIÓN
